882c16fe7b987e3250db68c02390ed3526d7bd2e
Paul Jaworski <pjaworski27@gmail.com>
2015-04-23 11:55:37 -0700

    passing data when changing routes

--- index.ios.js
+++ index.ios.js
@@ -1,54 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+var Main = require('./App/Components/Main');
+var Dashboard = require('./App/Components/Dashboard');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View,
 } = React;
 
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  },
+});
+
 class githubNotetaker extends React.Component{
   render() {
-    var lol;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Notetaker!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+      style={styles.container}
+        initialRoute={{
+          title: 'Github NoteTaker',
+          component: Main
+        }} />
     );
   }
 };
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
